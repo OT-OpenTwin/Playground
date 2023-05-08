3082caf1d60be219b72b7f015b8c56174d8f2a09
alexk95 <57009007+alexk95@users.noreply.github.com>
2023-05-08 15:41:25 +0200

    Fixed project settings for ui playground

--- AlexUIPlayground/edit.bat
+++ AlexUIPlayground/edit.bat
@@ -27,7 +27,7 @@ IF "%DEVENV_ROOT_2022%"=="" (
 ECHO Setting up environment
 
 REM Setup eviroment
-CALL "%SIM_PLAT_ROOT%\MasterBuild\set_env.bat"
+CALL "%OPENTWIN_DEV_ROOT%\Scripts\SetupEnvironment.bat"
 
 ECHO Launching development enviroment
 
--- AlexUIPlayground/src/MainWindow.cpp
+++ AlexUIPlayground/src/MainWindow.cpp
@@ -1,12 +1,6 @@
 #include "MainWindow.h"
 
 // OpenTwin header
-#include <openTwin/BlockNetworkEditor.h>
-#include <openTwin/BlockNetwork.h>
-#include <openTwin/Block.h>
-#include <openTwin/FlowBlock.h>
-#include <openTwin/FlowConnector.h>
-#include <openTwin/FlowConnection.h>
 
 // Qt header
 #include <QtCore/qsettings.h>
@@ -17,82 +11,18 @@ MainWindow * g_instance{ nullptr };
 
 #include <thread>
 
-ot::BlockNetworkEditor* g_editor;
 
-ot::FlowBlock* leftBlock;
-ot::FlowBlock* rightBlock;
 
 void MainWindow::createOwnWidgets(void) {
-	g_editor = new ot::BlockNetworkEditor;
+	//g_editor = new ot::BlockNetworkEditor;
 
-	// Left
-	leftBlock = new ot::FlowBlock("Calculate");
-	leftBlock->setTitleImage(QPixmap(QString(":/images/f.png")));
+	
 
-	ot::FlowConnector* leftOut0 = new ot::FlowConnector("Start", ot::OutputFlow, ot::Triangle);
-	leftOut0->setFillColor(QColor(128, 128, 128));
-	leftOut0->setBorderColor(QColor(255, 255, 255));
-
-	ot::FlowConnector* leftOut1 = new ot::FlowConnector("Initial Data", ot::OutputFlow, ot::Circle);
-	leftOut1->setFillColor(QColor(0, 255, 128));
-
-	leftBlock->addConnector(leftOut0);
-	leftBlock->addConnector(leftOut1);
-
-	if (false) {
-		for (int i = 0; i < 5; i++) {
-			ot::FlowConnector* leftOut2 = new ot::FlowConnector("Some stuff " + QString::number(i), ot::OutputFlow, ot::Triangle);
-			leftOut2->setFillColor(QColor(68, 255, 68));
-			leftBlock->addConnector(leftOut2);
-		}
-	}
-
-	// Right
-	rightBlock = new ot::FlowBlock("Calculate stuff");
-	rightBlock->setTitleColor(QColor(128, 0, 255));
-
-	ot::FlowConnector* rightIn0 = new ot::FlowConnector("Run", ot::InputFlow, ot::Triangle);
-	rightIn0->setFillColor(QColor(128, 128, 128));
-	rightIn0->setBorderColor(QColor(255, 255, 255));
-
-	ot::FlowConnector* rightIn1 = new ot::FlowConnector("Data", ot::InputFlow, ot::Circle);
-	rightIn1->setFillColor(QColor(0, 255, 128));
-
-	ot::FlowConnector* rightOut0 = new ot::FlowConnector("Success", ot::OutputFlow, ot::Triangle);
-	rightOut0->setFillColor(QColor(128, 128, 128));
-	rightOut0->setBorderColor(QColor(255, 255, 255));
-
-	ot::FlowConnector* rightOut1 = new ot::FlowConnector("Fail", ot::OutputFlow, ot::Triangle);
-	rightOut1->setFillColor(QColor(128, 128, 128));
-	rightOut1->setBorderColor(QColor(255, 255, 255));
-
-	ot::FlowConnector* rightOut2 = new ot::FlowConnector("ResultA", ot::OutputFlow, ot::Circle);
-	rightOut2->setFillColor(QColor(255, 0, 128));
-
-	ot::FlowConnector* rightOut3 = new ot::FlowConnector("ResultB", ot::OutputFlow, ot::Circle);
-	rightOut3->setFillColor(QColor(255, 128, 0));
-
-	rightBlock->addConnector(rightIn0);
-	rightBlock->addConnector(rightIn1);
-
-	rightBlock->addConnector(rightOut0);
-	rightBlock->addConnector(rightOut1);
-	rightBlock->addConnector(rightOut2);
-	rightBlock->addConnector(rightOut3);
-
-	// Fill network
-	g_editor->network()->addBlock(leftBlock, QPoint(-80, 0));
-	g_editor->network()->addBlock(rightBlock, QPoint(80, 0));
-
-	// Create connections
-	g_editor->network()->registerConnection(leftOut0->connectTo(rightIn0));
-	g_editor->network()->registerConnection(leftOut1->connectTo(rightIn1));
-
-	m_tabWidget->addTab(g_editor, "Block Editor");
+	//m_tabWidget->addTab(g_editor, "Block Editor");
 }
 
 void MainWindow::test(void) {
-	g_editor->network()->update();
+	//g_editor->network()->update();
 }
 
 // ^ Add custom code above ^
